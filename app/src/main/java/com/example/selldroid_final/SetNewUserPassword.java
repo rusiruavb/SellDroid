fe907257b5f046ec94c180595587370fb5eb91b8
Jayalakshi <66734518+Jayalakshi@users.noreply.github.com>
2020-08-14 13:11:16 +0530

    commit from jaye

---
+++ app/src/main/java/com/example/selldroid_final/SetNewUserPassword.java
@@ -0,0 +1,14 @@
+package com.example.selldroid_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetNewUserPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_new_user_password);
+    }
+}
