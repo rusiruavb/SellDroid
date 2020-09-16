41fa75e865b4fff0382ab6295f0baeaed53ba9c4
Rusiru Abhisheak <rusiruavb98@gmail.com>
2020-09-16 10:38:11 +0530

    RAVB - update 16-09-2020

--- app/src/main/java/com/example/selldroid_final/AddProduct.java
+++ app/src/main/java/com/example/selldroid_final/AddProduct.java
@@ -7,12 +7,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class AddProduct extends Fragment {
+
+    private EditText productName;
+    private EditText price;
+    private EditText quantity;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_product, container, false);
+        productName = view.findViewById(R.id.add_product_name);
+        price = view.findViewById(R.id.add_product_price);
+        quantity = view.findViewById(R.id.add_product_quantity);
         return view;
     }
 }
