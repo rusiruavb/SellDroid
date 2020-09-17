ee4c8c9c5673a5ef5be45fd845460c0fa71edbea
Nithya980711 <nithyadesilva98@gmail.com>
2020-09-17 10:49:32 +0530

    NITH-seller registration backend updated

---
+++ app/src/main/java/com/example/selldroid_final/Seller.java
@@ -0,0 +1,14 @@
+package com.example.selldroid_final;
+
+import android.widget.EditText;
+
+public class Seller {
+
+    private String sellerName;
+    private String sellerEmail;
+    private String shopName;
+    private EditText shopAddress;
+    private EditText phoneNumber;
+    private EditText password;
+    private EditText confirmPassword;
+}
